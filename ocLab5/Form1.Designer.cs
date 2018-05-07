e6c9424ff7c2e03399ced5282b60fbb80b942aca
prepared06 <ironbullyk@gmail.com>
2018-05-07 22:35:20 +0300

    first commit

---
+++ ocLab5/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ocLab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 29);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(63, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Кольор";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(69, 29);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(63, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Форма";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(145, 29);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(66, 17);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "Локація";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Location = new System.Drawing.Point(9, 198);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(217, 52);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightGray;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(236, 201);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 262);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Операційні системи";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+    }
+}
+
---
+++ ocLab5/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ocLab5
+{
+    public partial class Form1 : Form
+    {
+        static Mutex mutexObj;//объект мьютекс   
+        //потоки
+        Thread FORM;
+        Thread COLOR;
+        Thread LOCATION;
+        //переменная для управления поездок по экрану
+        int recLoc = 1;
+        Bitmap bmp;//файл картинки
+        Graphics graph;
+        //цвета кистей
+        Pen pen_red;//
+        Pen pen_blue;//
+        Pen pen_green;
+        //переменные координаты
+        int x1, y1, w,h;
+        public Form1()
+        {
+            InitializeComponent();
+            x1 = y1 = 78;
+            w = 80;
+            h = 20;
+            bmp = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height); //создаем новую картинку
+
+                graph = Graphics.FromImage(bmp);
+                pen_red = new Pen(Color.Red);
+                pen_blue = new Pen(Color.Blue);
+                pen_green = new Pen(Color.Green); 
+                graph.DrawRectangle(pen_red, x1, y1, w, h);
+                pictureBox1.Image = bmp;
+
+
+        }
+        private void Change_Form()//меняем форму, путём скрытие других фигур и открытие нужной
+        {
+            while (true)
+            {
+                mutexObj.WaitOne();//приостанавливает выполнение потока до тех пор, пока не будет получен мьютекс mutexObj   
+
+                graph.Clear(Color.LightGray);//очищаем экран
+                graph.DrawEllipse(pen_red, x1, y1, w, h);//рисуем элипс
+                pictureBox1.Image = bmp;//выводим картинку
+                Thread.Sleep(100);//задержка
+
+                graph.Clear(Color.LightGray);//очищаем экран
+                graph.DrawEllipse(pen_red, x1+30, y1, h, h);//рисуем круг
+                pictureBox1.Image = bmp;//выводим картинку 
+                Thread.Sleep(100);//задержка
+
+                graph.Clear(Color.LightGray);//очищаем экран
+                graph.DrawRectangle(pen_red, x1, y1, w, h);//рисуем прямоугольник
+                pictureBox1.Image = bmp;//выводим картинку
+                Thread.Sleep(100);//задержка
+
+                mutexObj.ReleaseMutex();//После выполнения всех действий, когда мьютекс больше не нужен, поток освобождает его с помощью метода mutexObj.ReleaseMutex()
+                
+            }
+        }
+        private void Change_Color()//меняем цвет сразу всем
+        {
+            
+                while (true)
+                {
+                    mutexObj.WaitOne();//приостанавливает выполнение потока до тех пор, пока не будет получен мьютекс mutexObj
+
+                    graph.DrawRectangle(pen_blue, x1, y1, w, h);//рисуем прямоугольник синего цвета
+                    pictureBox1.Image = bmp;//выводим
+                    Thread.Sleep(100);//задержка
+
+                    graph.DrawRectangle(pen_green, x1, y1, w, h);//рисуем прямоугольник зеленого цвета
+                    pictureBox1.Image = bmp;//выводим
+                    Thread.Sleep(100);//задержка
+
+                    graph.DrawRectangle(pen_red, x1, y1, w, h);//рисуем прямоугольник красного цвета
+                    pictureBox1.Image = bmp;//выводим
+                    Thread.Sleep(100);//задержка
+
+                    mutexObj.ReleaseMutex();//После выполнения всех действий, когда мьютекс больше не нужен, поток освобождает его с помощью метода mutexObj.ReleaseMutex()
+                }
+            
+        }
+        private void Change_Location()//меняем локацию фигуры
+        {
+            while (true)
+            {
+                mutexObj.WaitOne();//приостанавливает выполнение потока до тех пор, пока не будет получен мьютекс mutexObj
+                if (y1 == 1 | y1 == 180)//усливое изменения полярности движение
+                {
+                    recLoc *= -1;
+                }
+                y1 += recLoc;//инкремент 
+
+                graph.Clear(Color.LightGray);
+                graph.DrawRectangle(pen_red, x1, y1, w, h);
+                pictureBox1.Image = bmp;
+                Thread.Sleep(100);
+
+                mutexObj.ReleaseMutex();//После выполнения всех действий, когда мьютекс больше не нужен, поток освобождает его с помощью метода mutexObj.ReleaseMutex()
+            }
+        }
+        //свойство изменения checkBox цвета
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true & checkBox2.Checked == false & checkBox3.Checked == false)//условие создания мьютекса(баганутое место, нельзя выключать первонажатую галочку)
+            {
+                mutexObj = new Mutex();//инициализация объекта мьютекса
+            }
+            if (checkBox1.Checked)//если активировали
+            {
+                COLOR = new Thread(Change_Color);//инициализация потока
+                COLOR.Start();//запуск потока
+            }
+            else//если деактивировали
+            {
+                COLOR.Abort();//останавливаем поток
+            }
+        }
+        //свойство изменения checkBox формы
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == false & checkBox2.Checked == true & checkBox3.Checked == false)
+            {
+                mutexObj = new Mutex();
+            }
+            if (checkBox2.Checked)
+            {
+                FORM = new Thread(Change_Form);
+                FORM.Start();
+            }
+            else
+            {
+                FORM.Abort();
+            }
+        }
+        //свойство изменения checkBox координаты
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == false & checkBox2.Checked == false & checkBox3.Checked == true)
+            {
+                mutexObj = new Mutex();
+            }
+            if (checkBox3.Checked)
+            {
+                LOCATION = new Thread(Change_Location);
+                LOCATION.Start();
+            }
+            else
+            {
+                LOCATION.Abort();
+            }
+        }
+
+
+
+    }
+}
